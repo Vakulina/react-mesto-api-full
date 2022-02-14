6d4e6084325f9481019a280bfcb56f39f7122532
Vakulina <viktoria.ganina@yandex.ru>
2022-02-15 00:09:01 +0300

    buils first version of deploy(backend part)

--- fronend/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copiright">© 2021. Mesto Russia</p>
-    </footer>
-  );
-}
-export default Footer;
--- fronend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({ component: Component, ...props }) {
-  return (
-    props.loggedIn ? <Component {...props} /> : <Navigate to="/login" />
-  );
-};
-export default ProtectedRoute;
